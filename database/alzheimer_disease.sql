5ca15fc279459ec046012e4402c7c0154a36fd42
Surya <122272821+sj2064@users.noreply.github.com>
2024-12-09 20:49:31 +0530

    Add files via upload

---
+++ database/alzheimer_disease.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.6
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 12, 2023 at 06:10 AM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `alzheimer_disease`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`username`, `password`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `register`
+--
+
+CREATE TABLE `register` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `dob` varchar(20) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `uname` varchar(20) NOT NULL,
+  `pass` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `register`
+--
+
+INSERT INTO `register` (`id`, `name`, `gender`, `dob`, `address`, `email`, `mobile`, `uname`, `pass`) VALUES
+(1, 'Ramesh', 'Male', '1998-06-22', 'Chennai', 'ramesh@gmail.com', 8567412548, 'ramesh', '1234');
